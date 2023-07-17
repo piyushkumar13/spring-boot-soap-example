af0bf73db034fdde262e1a66f03736cb663711cb
piyushkumar13 <piyush.k9013@gmail.com>
2023-07-17 16:17:52 +0530

    initial commit

---
+++ soap-server/src/test/java/com/example/soapserver/SoapServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.soapserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
